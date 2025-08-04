a9676ffb5d91e86418a0b2484942fd94d78bdd26
reza-ya <rezaayaari@gmail.com>
2025-08-04 19:35:54 +0330

    feat: write first line of the avl insert

--- DataStructure_Algorithm_Csharp/Tree/AVLTree.cs
+++ DataStructure_Algorithm_Csharp/Tree/AVLTree.cs
@@ -6,8 +6,35 @@ using System.Threading.Tasks;
 
 namespace DataStructure_Algorithm_Csharp.Tree
 {
-    public class AVLTree
+    public class AVLTree<T>
     {
+        private Node? _root;
+        public void Add(T item)
+        {
+            var newNode = new Node
+            {
+                weight = 0,
+                Value = item,
+                Left = null,
+                Right = null
+            };
 
+            if (_root == null)
+            {
+                _root = newNode;
+            }
+
+
+        }
+
+
+
+        public class Node
+        {
+            public int weight;
+            public T? Value;
+            public Node? Left;
+            public Node? Right;
+        }
     }
 }
