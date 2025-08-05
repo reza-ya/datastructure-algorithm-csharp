738da0031caf4f46b74fb1abd9060b283d843445
reza-ya <rezaayaari@gmail.com>
2025-08-05 22:42:29 +0330

    feat: middle of avl tree

--- DataStructure_Algorithm_Csharp/Program.cs
+++ DataStructure_Algorithm_Csharp/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace DataStructure_Algorithm_Csharp
+﻿using DataStructure_Algorithm_Csharp.Tree;
+
+namespace DataStructure_Algorithm_Csharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var avlTree = new AVLTree();
+
+            avlTree.Add(5);
+            avlTree.Add(2);
+            avlTree.Add(1);
+            avlTree.Add(3);
+            avlTree.Add(7);
+            avlTree.Add(6);
+            //avlTree.Add(8);
+
+            var root = avlTree.GetRoot();
             Console.WriteLine("Hello, World!");
         }
     }
--- DataStructure_Algorithm_Csharp/Tree/AVLTree.cs
+++ DataStructure_Algorithm_Csharp/Tree/AVLTree.cs
@@ -1,40 +1,135 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataStructure_Algorithm_Csharp.Tree
 {
-    public class AVLTree<T>
+    public class AVLTree
     {
         private Node? _root;
-        public void Add(T item)
+        private int _count = 0;
+        public void Add(int value)
         {
             var newNode = new Node
             {
+                Id = _count++,
                 weight = 0,
-                Value = item,
+                Value = value,
                 Left = null,
-                Right = null
+                Right = null,
+                Parrent = null,
             };
 
             if (_root == null)
             {
                 _root = newNode;
+                return;
             }
 
+            var currentNode = _root;
+            while (newNode.Parrent == null && currentNode != null)
+            {
 
+                if (newNode.Value > currentNode.Value)
+                {
+                    if (currentNode.Right == null)
+                    {
+                        // insert to the right of the current node
+                        currentNode.Right = newNode;
+                        newNode.Parrent = currentNode;
+                        this.UpdateWeightBackTracking(newNode);
+                    }
+                    else
+                    {
+                        currentNode = currentNode.Right;
+                    }
+                }
+                else
+                {
+
+                    if (currentNode.Left == null)
+                    {
+                        // insert to the left of the current node
+                        currentNode.Left = newNode;
+                        newNode.Parrent = currentNode;
+                        this.UpdateWeightBackTracking(newNode);
+                    }
+                    else
+                    {
+                        currentNode = currentNode.Left;
+                    }
+                }
+            }
+
+        }
+
+        private void UpdateWeightBackTracking(Node leaf)
+        {
+            var currentNode = leaf;
+            //bool isLeft = currentNode?.Parrent?.Left?.Id == currentNode.Id;
+            //bool isRight = currentNode?.Parrent?.Right?.Id == currentNode.Id;
+            //if (isLeft)
+            //{
+                while (currentNode != null)
+                {
+                    bool isLeft = currentNode?.Parrent?.Left?.Id == currentNode.Id;
+                    bool isRight = currentNode?.Parrent?.Right?.Id == currentNode.Id;
+                if (currentNode.Parrent.weight > 0)
+                    {
+                        currentNode.Parrent.weight--;
+                        break;
+                    }
+                    if (currentNode.Parrent.weight < 0)
+                    {
+                        currentNode.Parrent.weight++;
+                        break;
+                }
+                    currentNode.Parrent.weight--;
+                    currentNode = currentNode?.Parrent;
+                    isLeft = currentNode?.Parrent?.Left?.Id == currentNode.Id;
+                }
+            //}
+            //if (isRight)
+            //{
+            //    while (currentNode != null && isRight)
+            //    {
+            //        if (currentNode.Parrent.weight < 0)
+            //        {
+            //            currentNode.Parrent.weight++;
+            //            break;
+            //        }
+            //        currentNode.Parrent.weight++;
+            //        currentNode = currentNode?.Parrent;
+            //        isRight = currentNode?.Parrent?.Right?.Id == currentNode.Id;
+            //    }
+            //}
+
+        }
+
+        public Node? GetRoot()
+        {
+            return _root;
+        }
+
+        public int Count()
+        {
+            return _count;
         }
 
 
 
         public class Node
         {
+            public int Id { get; init; }
             public int weight;
-            public T? Value;
+            public int Value;
             public Node? Left;
             public Node? Right;
+            public Node? Parrent;
         }
     }
 }
